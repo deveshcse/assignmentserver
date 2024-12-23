97e108e4802b44ac2bd91a2cf40f76d34bd7d77c
deveshcse <d.mishra56603@gmail.com>
2024-12-24 00:02:03 +0530

    completed assingment backend project

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+
+const backend_port = process.env.BACKEND_PORT || 3000;
+app.use(cors({
+    origin: '*', // Allow requests from any origin
+    methods: '*', // Allow all HTTP methods
+    allowedHeaders: '*' // Allow all headers
+}));
+
+const crudRouter = require('./routes/studentsRoutes');
+
+app.use('/api', crudRouter)
+
+app.get('/', (req, res) =>{
+    res.send("Hi from Server...");
+})
+
+
+app.listen(backend_port, () => {
+    console.log(`server is running on port ${backend_port}`);
+    
+})
